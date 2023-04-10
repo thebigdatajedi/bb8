819e908701c45a0c3a745b51d7c1e6817f0b762c
Gabe Cruz <gabe.cruz@henryscheinone.com>
2023-04-10 04:48:13 -0600

    got the functionality I needed in the project, later I will add the better version of the README.md file that I created and that will be that for a little while.

--- bb8.go
+++ bb8.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -77,7 +78,6 @@ func CreateInfoPlistFile(pListcontent InfoPListFileContent) {
 	types := values.Type()
 
 	for i := 0; values.NumField() > i; i++ {
-		//_, err = infoPlistFile.WriteString("<key>" + i + "</key>" + "\n" + "<string>" + s + "</string>")
 		_, err = infoPlistFile.WriteString(types.Field(i).Name + "\n" + values.Field(i).String() + "\n")
 	}
 
@@ -85,10 +85,19 @@ func CreateInfoPlistFile(pListcontent InfoPListFileContent) {
 
 } //end of CreateInfoPlistFile
 
+// Context::
+// Lots of comments is usually a sign of code smell.
+// There are exceptions to every rule, there are exceptions to this rule too.
+// The steps in copyResource use a concept of a Reader and a Writer that I've not had to reason about before.
+// In experiences with other languages the source resource and the destination resources were both strings.
+// If you look at this code I had to do the os.Open so that fileToCopy would have a Reader that io.Copy could use.
+// I also had to do the os.Create so that dst would have a Writer that io.Copy could use.
+// I'd like to keep this context text and comments in the function for a little while,
+// while I gel a little more with this concept.
 func copyResource(sourceFile string, targetDir string) {
-	//take the string reference of icon.icsn and get *File type that has  Reader that
+	//take the string reference of fileToCopy.icsn and get *File type that has  Reader that
 	//has a reader that io.Copy can use for the copy operation.
-	icon, err := os.Open(sourceFile)
+	fileToCopy, err := os.Open(sourceFile)
 	StandardErrHandler(err)
 
 	//take the target dir to Create a *File object that has a Writer that io.Copy
@@ -96,12 +105,13 @@ func copyResource(sourceFile string, targetDir string) {
 	dst, err := os.Create(filepath.Join(targetDir, filepath.Base(sourceFile)))
 	StandardErrHandler(err)
 
-	//do the actual copy operation with icon's reader and dst's writer.
-	_, err = io.Copy(dst, icon)
+	//do the actual copy operation with fileToCopy's reader and dst's writer.
+	_, err = io.Copy(dst, fileToCopy)
 	StandardErrHandler(err)
 } //end of copyResource
 
-func CreateAppBundleStructure(appNameParam string, projectRootDirParam string, newBinaryNameParam string, pListcontentParam InfoPListFileContent) {
+func CreateAppBundleStructure(appNameParam string, projectRootDirParam string, newBinaryNameParam string,
+	bb8IcnsResourceLocationSourceParam string, pListcontentParam InfoPListFileContent) {
 	appBundleStructure := []string{appNameParam, "Contents", "MacOS", "Resources"}
 	for _, s := range appBundleStructure {
 		err := os.Mkdir(s, 0750)
@@ -123,10 +133,11 @@ func CreateAppBundleStructure(appNameParam string, projectRootDirParam string, n
 
 		if s == "Resources" {
 			targetDir := "."
-			sourceFile := "/Users/gabe.cruz/wrk_cool/bb8/resources/bb8.icns"
+			sourceFile := bb8IcnsResourceLocationSourceParam
 			copyResource(sourceFile, targetDir)
 		}
 	} //appBundleStructure for loop
+
 	//the file structure is built
 	//cd back to project root dir
 	err := os.Chdir(projectRootDirParam)
@@ -139,7 +150,7 @@ func BuildApp(appNameParam string, newBinaryNameParam string, err error) {
 	//now run the command line tool to create the binary
 	newBinaryNameTargetPath := filepath.Join(appNameParam, "Contents", "MacOS", newBinaryNameParam)
 	cmd := exec.Command("go", "build", "-o", newBinaryNameTargetPath)
-	cmd.Stdin = strings.NewReader("main.go")
+	cmd.Stdin = strings.NewReader("bb8.go")
 	var out strings.Builder
 	cmd.Stdout = &out
 	err = cmd.Run()
@@ -150,18 +161,22 @@ func BuildApp(appNameParam string, newBinaryNameParam string, err error) {
 } //BuildApp
 
 func Usage() {
-	log.Println("USAGE::")
-	log.Println("CREATE FastAPI project::")
-	log.Println("cd into directory where you want to create your project and type::")
-	log.Println("main create_fapi_project <project_name> the oi stands for 'create project from outside in'.")
-	log.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-	log.Println("ORGANIZE existing FastAPI project::")
-	log.Println("or cd into already existing <project_name> dir and type:: main organize_fapi_project which stands for " +
-		"'create project from inside out'.")
-	log.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-	log.Println("BUILD APP for macOS::")
-	log.Println("main buildApp <appName> <projectRootDir> <newBinaryName>")
-	log.Println("No parameters passed reports Usage.")
+	fmt.Println("USAGE::")
+	fmt.Println("1. bb8 - with no parameters passed reports usage.")
+	fmt.Println("CREATE FastAPI project::")
+	fmt.Println("1. cd into directory where you want to create your FastAPI project and type::")
+	fmt.Println("2. bb8 create_fapi_project <project_name> - 'create fapi project'.")
+	fmt.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+	fmt.Println("ORGANIZE existing FastAPI project::")
+	fmt.Println("1. cd into already existing <project_name> dir and type::")
+	fmt.Println("2. bb8 organize_fapi_project - 'organize fap project'.")
+	fmt.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+	fmt.Println("BUILD GUI APP for macOS::")
+	fmt.Println("1. bb8 bundle")
+	fmt.Println("2. Respond to the questions asked by the program.")
+	fmt.Println("3. The program will create a .app bundle in the current directory.")
+	fmt.Println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
 	return
 } //end of Usage
 
@@ -183,69 +198,88 @@ type InfoPListFileContent struct {
 } //end of InfoPListFileContent struct
 
 func main() {
+	//Shopping List::
+	//todo [*]Parameterize the Info.plist file content
+	//todo [*]rename bundleApp to bundleSelf
+	//todo [*] add bundleApp to bundle other applications that have a UI.
+	//todo [*] change usage to print to screen and not log to screen.
+	//todo [] add flags to the command line tool and remove the action label style of passing parameters.
+	//todo [*] make usage easier to read and use.
+	//todo [*] message the user better when a cli action has completed.
+	//todo [] configure bb8 so that logging goes to screen and to a log file all the time and make that configuration easy to change via flag and config file.
 	var appName string
 	var projectRootDir string
 	var newBinaryName string
 	var appNameDotApp string
+	var bb8IcnsName string
+	var bb8IcnsBundleLocationTarget string
+	var bb8IcnsResourceLocationSourceDirectory string
+	var bb8IcnsResourceLocationSource string
+	var bundleIDPrefix string
+	var bundleID string
+	var bundleInfoDictionaryVersion string
+	var bundlePackageType string
+	var bundleShortVersionString string
+	var bundleVersion string
 
-	infoPListFileContent := InfoPListFileContent{
-		CFBundleDisplayName:           "MyApp",
-		CFBundleExecutable:            "Binary",
-		CFBundleIconFile:              "icon.icns",
-		CFBundleIdentifier:            "com.mycompany.myapp",
-		CFBundleInfoDictionaryVersion: "6.0",
-		CFBundlePackageType:           "APPL",
-		CFBundleShortVersionString:    "1.0",
-		CFBundleVersion:               "1",
-	}
-
-	//parameter checking engine
 	numOfParameters := len(os.Args)
 	if numOfParameters > 1 {
 		if os.Args[1] == "organize_fapi_project" {
 			CreateRootDirLevelFiles()
 			CreateRootLevelPythonModules()
 		} else if os.Args[1] == "create_fapi_project" {
-			if true {
+			if true { //this is checking if projectName is not empty
 				CreateProjectRootDir(os.Args[2])
 			}
-		} else if os.Args[1] == "bundleApp" {
-			//Create the app bundle
-			if numOfParameters == 4 { //For this code to run the 4 parameters in args are:
-				//1. "bundleApp" value
-				//2. "appName" variable
-				//3. "projectRootDir" variable
-				//4. "newBinaryName" variable
-				if true { //if bundleApp request is not empty
-					if true { //if appName is not empty
-						appName = os.Args[2]
-					}
-					if true { //if projectRootDir is not empty
-						projectRootDir = os.Args[3]
-					}
-					if true { //if newBinaryName is not empty
-						newBinaryName = os.Args[4]
-					}
+		} else if os.Args[1] == "bundle" {
+			//gather data for buildApp function
+			fmt.Println("Enter your appName: ")
+			fmt.Scanln(&appName)
+			fmt.Println("Enter your projectRootDir: ")
+			fmt.Scanln(&projectRootDir)
+			fmt.Println("Enter your newBinaryName: ")
+			fmt.Scanln(&newBinaryName)
+			fmt.Println("Enter your bb8IcnsName: ")
+			fmt.Scanln(&bb8IcnsName)
+			fmt.Println("Enter your bundleIDPrefix: ")
+			fmt.Scanln(&bundleIDPrefix)
+			fmt.Println("Enter your bb8IcnsResourceLocationSourceDirectory: ")
+			fmt.Scanln(&bb8IcnsResourceLocationSourceDirectory)
 
-					appNameDotApp = appName + ".app"
+			fmt.Println("appName: ", appName)
+			fmt.Println("projectRootDir: ", projectRootDir)
+			fmt.Println("newBinaryName: ", newBinaryName)
+			fmt.Println("bb8IcnsName: ", bb8IcnsName)
+			fmt.Println("bundleIDPrefix: ", bundleIDPrefix)
+			fmt.Println("bb8IcnsResourceLocationSourceDirectory: ", bb8IcnsResourceLocationSourceDirectory)
 
-					CreateAppBundleStructure(appNameDotApp, projectRootDir, newBinaryName, infoPListFileContent)
-					//shopping list:
-					//[*]create a function that will take appName, projectRootDir, and newBinaryName
-					//[*] run from the command line the following command::
-					//go build -o ${appName}.app/Contents/MacOS/${YourGoBinary} main.go
-					//Additonal Research Notes::
-					//This guy is saying to bootstrap the project root dir by passing a
-					//a project root dir and then he shares some source that makes it look like he's
-					//using something called a flag and it looks like the way it can be used is to
-					//ask the user for parameters in the command line.
-					//https://stackoverflow.com/questions/47531760/how-to-get-the-root-path-of-the-project
-					//Copilot Notes::
-					//https://stackoverflow.com/questions/28322997/how-to-pass-arguments-to-go-program
-					//The top one is the one I found the second one is one that Copilot suggested.
-				}
+			//dynamic Info.plist file content - from parameters passed to the command line
+			bundleID = bundleIDPrefix + "." + appName
+			appNameDotApp = appName + ".app"
+			bb8IcnsBundleLocationTarget = appNameDotApp + "/Contents/Resources/" + bb8IcnsName
+			bb8IcnsResourceLocationSource = bb8IcnsResourceLocationSourceDirectory + "/" + bb8IcnsName
+
+			//static Info.plist file content
+			bundleInfoDictionaryVersion = "6.0"
+			bundlePackageType = "APPL"
+			bundleShortVersionString = "1.0"
+			bundleVersion = "1"
+
+			infoPListFileContent := InfoPListFileContent{
+				CFBundleDisplayName:           appName,
+				CFBundleExecutable:            newBinaryName,
+				CFBundleIconFile:              bb8IcnsBundleLocationTarget,
+				CFBundleIdentifier:            bundleID,
+				CFBundleInfoDictionaryVersion: bundleInfoDictionaryVersion,
+				CFBundlePackageType:           bundlePackageType,
+				CFBundleShortVersionString:    bundleShortVersionString,
+				CFBundleVersion:               bundleVersion,
 			}
+
+			CreateAppBundleStructure(appNameDotApp, projectRootDir, newBinaryName, bb8IcnsResourceLocationSource, infoPListFileContent)
+			log.Println("App bundle created successfully!!!")
 		}
+
 	} else {
 		Usage()
 	}
